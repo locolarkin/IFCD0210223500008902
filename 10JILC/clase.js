e1d1d162cc82b865ef8a28e394b7b6933fa20680
Hose Lecoco <joselecoco@gmail.com>
2023-07-17 19:48:28 +0100

    algo

--- 10JILC/clase.js
+++ 10JILC/clase.js
@@ -1,9 +1,10 @@
-var = 2;
+const p = 3.14;
+var A = Juan;
 console.log( "A vale " + a);
-var b = 5;
+var b = José;
 console.log("B vale " + b);
 c = a;
 a = b;
 b = c;
-console.log("Ahora A vale " + a);
-console.log("Ahora B vale " + b);
+console.log("Ahora A se llama " + a);
+console.log("Ahora B se llama " + b);
